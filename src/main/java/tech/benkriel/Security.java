b6882a4c666dc19aeb38125a672613335356d761
Benito15 <b.kriel34@gmail.com>
2019-09-23 07:56:37 +0200

    Basic security

---
+++ src/main/java/tech/benkriel/Security.java
@@ -0,0 +1,13 @@
+package tech.benkriel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Security {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Security.class, args);
+    }
+
+}
